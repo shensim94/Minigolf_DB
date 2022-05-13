2fc067c8c474a13948c0ebbdff9ef8f502e282c1
hillyeal <hillyeal@oregonstate.edu>
2022-05-12 21:20:12 -0700

    add inner joins, update club_id

--- DML.sql
+++ DML.sql
@@ -2,19 +2,38 @@
 SELECT * FROM players ORDER BY player_id ASC;
 
 --Query for all club_members
-SELECT club_members.player_id AS "Player ID", players.name AS "Name", club_members.course_id AS "Course ID", clubs.name AS "Club Name" FROM club_members 
+SELECT club_members.player_id AS "Player ID", players.name AS "Name", club_members.club_id AS "Course ID", clubs.name AS "Club Name" FROM club_members 
 INNER JOIN players on players.player_id = club_members.player_id
-INNER JOIN clubs on clubs.course_id = club_members.course_id;
+INNER JOIN clubs on clubs.club_id = club_members.club_id;
 
 --Query for all clubs
-SELECT * FROM clubs ORDER BY course_id ASC;
+SELECT * FROM clubs ORDER BY club_id ASC;
 
 --Query for all holes
-SELECT * FROM holes ORDER BY course_id ASC;
+SELECT * FROM holes ORDER BY club_id ASC, hole_number ASC;
+
+--Query for all holes at a certain course
+SELECT * FROM holes WHERE club_id = :club_id_input ORDER BY hole_number ASC;
 
 --Query for all hole_scores
---best score at top
-SELECT * FROM hole_scores ORDER BY player_id ASC, score ASC;
+SELECT hole_scores.hole_scores_id,
+hole_scores.date,
+hole_scores.player_id,
+players.name,
+hole_scores.club_id,
+clubs.name,
+hole_scores.hole_number,
+hole_scores.score FROM hole_scores
+INNER JOIN players on players.player_id = hole_scores.player_id
+INNER JOIN clubs on clubs.club_id = hole_scores.club_id;
+
+--Query for all hole scores
+--for a player on a certain day
+SELECT hole_scores.date, hole_scores.club_id, clubs.name AS "club_name", hole_scores.player_id, players.name AS "player_name", hole_scores.hole_number,
+hole_scores.score FROM hole_scores
+INNER JOIN clubs on clubs.club_id = hole_scores.club_id
+INNER JOIN players on players.player_id = hole_scores.player_id
+WHERE hole_scores.date = :date_input AND hole_scores.player_id = :player_input;
 
 --Insert player
 INSERT INTO players
@@ -49,22 +68,21 @@ city=:city_input,
 state=:state_input 
 WHERE player_id = :player_id_from_select;
 
-
 --Insert club_members
 INSERT INTO club_members
 (
     player_id,
-    course_id
+    club_id
 )
 VALUES
 (
     :player_id_from_select,
-    :course_id_from_select
+    :club_id_from_select
 );
 
 --Delete club_members
 --should only delete a single membership.
-DELETE from club_members WHERE player_id = :player_id_from_select AND course_id= :course_id_from_select;
+DELETE from club_members WHERE player_id = :player_id_from_select AND club_id= :club_id_from_select;
 
 --(Not including update as we'd only add or remove.)
 
@@ -73,7 +91,7 @@ INSERT INTO hole_scores
 (
     date,
     player_id,
-    course_id,
+    club_id,
     hole_number,
     score
 )
@@ -81,7 +99,7 @@ VALUES
 (
     :current_date,
     :player_id_from_current_selected,
-    :course_id_from_current_selected,
+    :club_id_from_current_selected,
     :hole_number_from_current_increment,
     :score_input
 );
@@ -90,33 +108,30 @@ VALUES
 --'next_hole' button that might increment some variable.
 
 --Delete hole_scores
-DELETE FROM hole_scores WHERE date = :selected_date AND player_id = :player_id_from_select
-AND course_id = :course_id_from_select AND hole_number = :hole_number_from_select;
+DELETE FROM hole_scores WHERE hole_scores_id = :hole_score_id_input;
 
 --Update hole_scores
---updating a hole score may change its identifier.
---additionally, 
+--user will select a hole_score from a menu.
 UPDATE hole_scores
 SET
 date = :new_date,
 player_id = :new_id,
-course_id = :new_course_id,
+club_id = :new_club_id,
 hole_number = :new_hole_number,
 score = :new_score
-WHERE (date = :selected_date AND player_id = :player_id_from_select
-AND course_id = :course_id_from_select AND hole_number = :hole_number_from_select);
+WHERE hole_scores_id = :hole_score_id_from_select;
 
 --Insert holes
 INSERT INTO holes
 (
-    course_id,
+    club_id,
     hole_number,
     par_score,
     description
 )
 VALUES
 (
-    :course_id_from_select,
+    :club_id_from_select,
     :hole_number_from_validated,
     :par_score_input,
     :description_input
@@ -126,17 +141,16 @@ VALUES
 --written to verify that the user is not inserting duplicate holes, skipping holes, etc.
 
 --Delete holes
-DELETE FROM holes WHERE hole_number = :hole_number_from_select AND course_id = :course_id_from_select;
+DELETE FROM holes WHERE hole_number = :hole_number_from_select AND club_id = :club_id_from_select;
 
 --Update holes
 UPDATE holes
 SET
 hole_number= :new_hole_number,
-course_id = :new_course_id,
+club_id = :new_club_id,
 par_score = :new_par_score,
 description = :new_description
-WHERE course_id = :course_id_from_select AND hole_number = :hole_number_from_select;
-
+WHERE club_id = :club_id_from_select AND hole_number = :hole_number_from_select;
 
 --Insert clubs
 INSERT INTO clubs
@@ -159,7 +173,7 @@ VALUES
 );
 
 --Delete clubs
-DELETE FROM clubs WHERE course_id = :course_id_from_select;
+DELETE FROM clubs WHERE club_id = :club_id_from_select;
 
 --Update clubs
 UPDATE clubs
@@ -170,4 +184,4 @@ address = :new_address,
 zip_code = :new_zip,
 city = :new_city,
 state = :new_state
-WHERE course_id = :course_id_from_select;
+WHERE club_id = :club_id_from_select;
